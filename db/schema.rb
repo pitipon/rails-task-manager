0b9b800fe1e3ba47cc2db4096d23ce2dea185520
Pitipon Pluemworasawat <pitipon@gmail.com>
2017-11-21 13:54:34 +0800

    create controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121050124) do
+ActiveRecord::Schema.define(version: 20171121052711) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "todo"
-    t.boolean "done"
+    t.boolean "done", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "deadline"
