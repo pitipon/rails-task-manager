999eb733f460c373d04bcaf0ac2316d611d1e0b9
Pitipon Pluemworasawat <pitipon@gmail.com>
2017-11-21 16:19:43 +0800

    done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,56 @@
 class TasksController < ApplicationController
+  # GET: /tasks
   def index
+    @tasks = Task.all
+    @task = Task.new
   end
+
+  # GET: /tasks/:id
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  # GET: /tasks/new
+  def new
+
+  end
+
+  # POST: /tasks
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to root_path
+  end
+
+  # GET: /tasks/:id/edit
+  def edit
+    @task = Task.find(params[:id])
+
+  end
+
+  # PATCH: /tasks/:id
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to root_path
+  end
+
+  # DELETE: /restaurants/:id
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to root_path
+  end
+
+  private
+
+
+  def task_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:task).permit(:todo, :deadline)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
+  root to: 'tasks#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasks
+#   IN console type "rails routes"
+#   Prefix Verb   URI Pattern               Controller#Action
+#   root      GET    /                         tasks#index
+#   tasks     GET    /tasks(.:format)          tasks#index
+#             POST   /tasks(.:format)          tasks#create
+#   new_task  GET    /tasks/new(.:format)      tasks#new
+#   edit_task GET    /tasks/:id/edit(.:format) tasks#edit
+#   task      GET    /tasks/:id(.:format)      tasks#show
+#             PATCH  /tasks/:id(.:format)      tasks#update
+#             PUT    /tasks/:id(.:format)      tasks#update
+#             DELETE /tasks/:id(.:format)      tasks#destroy
+
 end
